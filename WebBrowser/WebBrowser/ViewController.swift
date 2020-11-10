70b3d79f671c57082f0bb1ae8b89f45940dabfb1
odong-Tree <ss12357@naver.com>
2020-11-10 18:32:12 +0900

    2. add openStartPage, showErrorAlert

--- WebBrowser/WebBrowser/ViewController.swift
+++ WebBrowser/WebBrowser/ViewController.swift
@@ -9,14 +9,28 @@ import WebKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var webKitView: WKWebView!
+    @IBOutlet weak var webView: WKWebView!
+    
+    let myUrl = "https://www.indiepost.co.kr"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        guard let url = URL(string: "https://www.indiepost.co.kr") else { return }
-        let request = URLRequest(url: url)
-        webKitView.load(request)
+        openStartPage()
+    }
+    
+    func openStartPage() {
+        guard let indiePostUrl = URL(string: myUrl) else { return }
+        let request = URLRequest(url: indiePostUrl)
+        webView.load(request)
+    }
+    
+    func showErrorAlert() {
+        let errorAlert = UIAlertController(title: "error", message: "url을 불러오는데 오류가 발생했습니다.", preferredStyle: .alert)
+        let OKButton = UIAlertAction(title: "OK", style: .default, handler: nil)
+        
+        errorAlert.addAction(OKButton)
+        present(errorAlert, animated: true, completion: nil)
     }
 
 }
